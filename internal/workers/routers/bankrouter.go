20c6f86de3d564ee447f158906673075b02143d1
Andres Colina <acolina@fi.uba.ar>
2026-05-14 20:10:41 -0300

    feat: Constructor de BankRouter

--- internal/workers/routers/bankrouter.go
+++ internal/workers/routers/bankrouter.go
@@ -1,17 +1,53 @@
 package routers
 
-import "github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+import (
+	"fmt"
+
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+)
 
 type BackRouterConfig struct {
-	MomHost           string
-	MomPort           int
-	InputQueueName    string
-	BankExchangerName string
-	MaxWorkersAmount  int
+	MomHost             string
+	MomPort             int
+	InputQueueName      string
+	MaxBankExchangeName string
+	MaxBankWorkerAmount int
 }
 
 type BankRouter struct {
-	inputQueue         middleware.Middleware
-	bankExchangerQueue middleware.ExchangeMiddleware
-	maxWorkersAmount   int
+	inputQueue          middleware.Middleware
+	maxBankExchange     *middleware.ExchangeMiddleware
+	maxBankExchangeKeys []string
+	maxWorkersAmount    int
+}
+
+func NewBankRouter(config BackRouterConfig) (*BankRouter, error) {
+	connSettings := middleware.ConnSettings{
+		Hostname: config.MomHost,
+		Port:     config.MomPort,
+	}
+
+	inputQueue, err := middleware.CreateQueueMiddleware(config.InputQueueName, connSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	maxBankExchangeKeys := make([]string, config.MaxBankWorkerAmount)
+	for i := range maxBankExchangeKeys {
+		maxBankExchangeKeys[i] = fmt.Sprintf("%s.%d", config.MaxBankExchangeName, i)
+	}
+
+	maxBankExchange, err := middleware.CreateExchangeMiddleware(config.MaxBankExchangeName, maxBankExchangeKeys, connSettings)
+	if err != nil {
+		// TODO: verificar error de cierre?
+		inputQueue.Close()
+		return nil, err
+	}
+
+	return &BankRouter{
+		inputQueue:          inputQueue,
+		maxBankExchange:     maxBankExchange,
+		maxBankExchangeKeys: maxBankExchangeKeys,
+		maxWorkersAmount:    config.MaxBankWorkerAmount,
+	}, nil
 }
