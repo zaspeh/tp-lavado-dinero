695703f0a65679d0cd2dc1fc1d77d6882ddf40b5
Andres Colina <acolina@fi.uba.ar>
2026-05-18 19:05:00 -0300

    chore: limpieza de helper por extarccion de errores

--- internal/factory/helpers.go
+++ internal/factory/helpers.go
@@ -1,7 +1,6 @@
 package factory
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,23 +9,6 @@ const (
 	defaultEnvString = ""
 	defaultEnvInt    = 0
 )
-
-type EnvError struct {
-	Key     string
-	Message string
-}
-
-func (e *EnvError) Error() string {
-	return fmt.Sprintf("%s: %s", e.Message, e.Key)
-}
-
-func NewEnvNotFoundError(key string) error {
-	return &EnvError{Key: key, Message: "Env var not found"}
-}
-
-func NewEnvNotNumericError(key string) error {
-	return &EnvError{Key: key, Message: "Env var is not a valid integer"}
-}
 
 func getEnvStrict(key string) (string, error) {
 	value, ok := os.LookupEnv(key)
