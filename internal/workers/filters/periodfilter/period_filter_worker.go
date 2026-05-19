19bbf9ece3c765a357ebf1a2e81370b222764b23
ArielFol <folgueiraariel@gmail.com>
2026-05-19 15:49:12 -0300

    feat: Constructor de PeriodFilter

--- internal/workers/filters/periodfilter/period_filter_worker.go
+++ internal/workers/filters/periodfilter/period_filter_worker.go
@@ -1,6 +1,8 @@
 package periodfilter
 
 import (
+	"errors"
+
 	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
 )
 
@@ -24,4 +26,78 @@ type PeriodFilterWorkerConfig struct {
 	MomHost                     string
 	MomPort                     int
 	Periods                     []Period
+}
+
+func NewPeriodFilterWorker(config PeriodFilterWorkerConfig) (*PeriodFilterWorker, error) {
+	if len(config.Periods) != len(config.AvgByTypeQueueNames) {
+		return nil, errors.New("period count must match avgByType queue count")
+	}
+
+	connSettings := middleware.ConnSettings{
+		Hostname: config.MomHost,
+		Port:     config.MomPort,
+	}
+
+	var createdQueues []middleware.Middleware
+
+	closeAllQueues := func() {
+		for _, queue := range createdQueues {
+			queue.Close()
+		}
+	}
+
+	usdInputQueue, err := middleware.CreateQueueMiddleware(config.UsdInputQueueName, connSettings)
+	if err != nil {
+		return nil, err
+	}
+	createdQueues = append(createdQueues, usdInputQueue)
+
+	rawInputQueue, err := middleware.CreateQueueMiddleware(config.RawInputQueueName, connSettings)
+	if err != nil {
+		closeAllQueues()
+		return nil, err
+	}
+	createdQueues = append(createdQueues, rawInputQueue)
+
+	var avgByTypeQueues []middleware.Middleware
+	for _, queueName := range config.AvgByTypeQueueNames {
+		queue, err := middleware.CreateQueueMiddleware(queueName, connSettings)
+		if err != nil {
+			closeAllQueues()
+			return nil, err
+		}
+		avgByTypeQueues = append(avgByTypeQueues, queue)
+		createdQueues = append(createdQueues, queue)
+	}
+
+	groupByOriginQueue, err := middleware.CreateQueueMiddleware(config.GroupByOriginQueueName, connSettings)
+	if err != nil {
+		closeAllQueues()
+		return nil, err
+	}
+	createdQueues = append(createdQueues, groupByOriginQueue)
+
+	groupByDestinationQueue, err := middleware.CreateQueueMiddleware(config.GroupByDestinationQueueName, connSettings)
+	if err != nil {
+		closeAllQueues()
+		return nil, err
+	}
+	createdQueues = append(createdQueues, groupByDestinationQueue)
+
+	paymentTypeFilterQueue, err := middleware.CreateQueueMiddleware(config.PaymentTypeFilterQueueName, connSettings)
+	if err != nil {
+		closeAllQueues()
+		return nil, err
+	}
+	createdQueues = append(createdQueues, paymentTypeFilterQueue)
+
+	return &PeriodFilterWorker{
+		usdInputQueue:           usdInputQueue,
+		rawInputQueue:           rawInputQueue,
+		avgByTypeQueues:         avgByTypeQueues,
+		groupByOriginQueue:      groupByOriginQueue,
+		groupByDestinationQueue: groupByDestinationQueue,
+		paymentTypeFilterQueue:  paymentTypeFilterQueue,
+		periods:                 config.Periods,
+	}, nil
 }
