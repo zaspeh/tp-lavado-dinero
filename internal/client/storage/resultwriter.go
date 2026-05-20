db404b076c7db5fe91c1cc1c0470149a0fc8f074
Andres Colina <acolina@fi.uba.ar>
2026-05-20 17:15:52 -0300

    feat: result writter para escribir las respuestas esperadas

---
+++ internal/client/storage/resultwriter.go
@@ -0,0 +1,80 @@
+package storage
+
+import (
+	"encoding/csv"
+	"os"
+	"path/filepath"
+
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/external/message/result"
+)
+
+const expectedEOFAmount = 5
+
+type ResultCSVWriter struct {
+	receivedEOFAmount int
+	outputDir         string
+	q1File            *os.File
+	q1Writer          *csv.Writer
+	q2File            *os.File
+	q2Writer          *csv.Writer
+}
+
+func NewResultCSVWriter(outputDir string) *ResultCSVWriter {
+	return &ResultCSVWriter{outputDir: outputDir}
+}
+
+func (rw *ResultCSVWriter) Open() error {
+	if err := os.MkdirAll(rw.outputDir, 0755); err != nil {
+		return err
+	}
+
+	var err error
+	rw.q1File, err = os.OpenFile(filepath.Join(rw.outputDir, "q1_result.csv"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	rw.q1Writer = csv.NewWriter(rw.q1File)
+
+	rw.q2File, err = os.OpenFile(filepath.Join(rw.outputDir, "q2_result.csv"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		rw.q1File.Close()
+		return err
+	}
+	rw.q2Writer = csv.NewWriter(rw.q2File)
+
+	return nil
+}
+
+func (rw *ResultCSVWriter) Close() {
+	//TODO: MANEJAR ERROR DE CLOSE
+	if rw.q1Writer != nil {
+		rw.q1Writer.Flush()
+	}
+	if rw.q1File != nil {
+		rw.q1File.Close()
+	}
+
+	if rw.q2Writer != nil {
+		rw.q2Writer.Flush()
+	}
+	if rw.q2File != nil {
+		rw.q2File.Close()
+	}
+}
+
+func (rw *ResultCSVWriter) DoneReceiving() bool {
+	return rw.receivedEOFAmount >= expectedEOFAmount
+}
+
+func (rw *ResultCSVWriter) HandleEOF(result result.EOF) error {
+	rw.receivedEOFAmount++
+	return nil
+}
+
+func (rw *ResultCSVWriter) HandleMaxBankResult(result result.MaxBankResult) error {
+	return nil
+}
+
+func (rw *ResultCSVWriter) HandleMicrotransactionResult(result result.MicrotransactionResult) error {
+	return nil
+}
