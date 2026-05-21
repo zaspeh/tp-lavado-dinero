cf60ad2178e594e85b77a38b7dc0493e95871882
ArielFol <folgueiraariel@gmail.com>
2026-05-20 23:25:35 -0300

    feat: agrega constructor de GroupByOriginWorker

--- internal/workers/groupers/origin/account_store.go
+++ internal/workers/groupers/origin/account_store.go
@@ -3,3 +3,9 @@ package origin
 type AccountStore struct {
 	data map[Account]map[Account]struct{}
 }
+
+func newAccountStore() *AccountStore {
+	return &AccountStore{
+		data: make(map[Account]map[Account]struct{}),
+	}
+}
