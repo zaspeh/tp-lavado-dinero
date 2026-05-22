e0ec4449bb34a66b00fa7073aec4d8492fec9543
Andres Colina <acolina@fi.uba.ar>
2026-05-22 11:44:13 -0300

    feat: manejo de sigteerm y sigint en filtro por formato

--- internal/workers/filters/formatfilter/format_filter_worker.go
+++ internal/workers/filters/formatfilter/format_filter_worker.go
@@ -1,6 +1,13 @@
 package formatfilter
 
-import "github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+)
 
 type FormatFilterConfig struct {
 	InputQueueName  string
@@ -11,9 +18,9 @@ type FormatFilterConfig struct {
 }
 
 type FormatFilterWorker struct {
-	inputQueue    middleware.Middleware
-	outputQuueue  middleware.Middleware
-	alloweFormats []string
+	inputQueue     middleware.Middleware
+	outputQueue    middleware.Middleware
+	allowedFormats []string
 }
 
 func NewFormatFilterWorker(cfg FormatFilterConfig) (*FormatFilterWorker, error) {
@@ -34,12 +41,32 @@ func NewFormatFilterWorker(cfg FormatFilterConfig) (*FormatFilterWorker, error) 
 	}
 
 	return &FormatFilterWorker{
-		inputQueue:    inputQueue,
-		outputQuueue:  outputQueue,
-		alloweFormats: cfg.AllowedFormats,
+		inputQueue:     inputQueue,
+		outputQueue:    outputQueue,
+		allowedFormats: cfg.AllowedFormats,
 	}, nil
 }
 
 func (w *FormatFilterWorker) Run() error {
+	go w.handleSignals()
+
+	w.inputQueue.StartConsuming(func(msg middleware.Message, ack, nack func()) {
+		// Aquí iría la lógica para procesar el mensaje y filtrar por formato
+		// Por ejemplo:
+		// 1. Deserializar el mensaje
+		// 2. Verificar si el formato del pago está en w.allowedFormats
+		// 3. Si es válido, enviar el mensaje a w.outputQueue
+		// 4. Acknowledge o Nack dependiendo del resultado
+	})
 	return nil
+}
+
+func (w *FormatFilterWorker) handleSignals() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+	slog.Info("shutdown signal received")
+	w.inputQueue.Close()
+	w.outputQueue.Close()
+
 }
