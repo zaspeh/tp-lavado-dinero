2cff56423fb7a03fd53c568944eb6331ada448a0
ArielFol <folgueiraariel@gmail.com>
2026-05-22 16:47:51 -0300

    feat: constructor IntermediaryRouter

--- internal/workers/routers/intermediaryrouter.go
+++ internal/workers/routers/intermediaryrouter.go
@@ -1,6 +1,8 @@
 package routers
 
 import (
+	"fmt"
+
 	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
 )
 
@@ -21,4 +23,34 @@ type IntermediaryRouterConfig struct {
 	AggregateByIntermediaryAmount int
 	MomHost                       string
 	MomPort                       int
+}
+
+func NewIntermediaryRouter(config IntermediaryRouterConfig) (*IntermediaryRouter, error) {
+	connSettings := middleware.ConnSettings{
+		Hostname: config.MomHost,
+		Port:     config.MomPort,
+	}
+
+	inputQueue, err := middleware.CreateQueueMiddleware(config.InputQueueName, connSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	aggregateByIntermediaryKeys := make([]string, config.AggregateByIntermediaryAmount)
+	for i := range aggregateByIntermediaryKeys {
+		aggregateByIntermediaryKeys[i] = fmt.Sprintf("%s.%d", config.AggregateByIntermediaryName, i)
+	}
+
+	aggregateByIntermediaryExchange, err := middleware.CreateExchangeMiddleware(config.AggregateByIntermediaryName, aggregateByIntermediaryKeys, connSettings)
+	if err != nil {
+		inputQueue.Close()
+		return nil, err
+	}
+
+	return &IntermediaryRouter{
+		inputQueue:                          inputQueue,
+		aggregateByIntermediaryExchange:     aggregateByIntermediaryExchange,
+		aggregateByIntermediaryWorkers:      config.AggregateByIntermediaryAmount,
+		aggregateByIntermediaryExchangeKeys: aggregateByIntermediaryKeys,
+	}, nil
 }
