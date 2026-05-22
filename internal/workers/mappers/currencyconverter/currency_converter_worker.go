fc941b307e642d893e295315270b94faa2680dc6
Andres Colina <acolina@fi.uba.ar>
2026-05-22 14:58:56 -0300

    feat: Logica base de worker conversion

--- internal/workers/mappers/currencyconverter/currency_converter_worker.go
+++ internal/workers/mappers/currencyconverter/currency_converter_worker.go
@@ -1,6 +1,16 @@
 package currencyconverter
 
-import "github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/protobuf"
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/serializer"
+)
 
 type CurrencyConverterConfig struct {
 	InputQueueName  string
@@ -41,5 +51,78 @@ func NewCurrencyConverterWorker(cfg CurrencyConverterConfig) (*CurrencyConverter
 }
 
 func (w *CurrencyConverterWorker) Run() error {
+	go w.handleSignals()
+
+	err := w.inputQueue.StartConsuming(func(msg middleware.Message, ack, nack func()) {
+		w.handleMessage(msg, ack, nack)
+	})
+
+	return err
+}
+
+func (w *CurrencyConverterWorker) handleSignals() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+	slog.Info("shutdown signal received")
+	w.inputQueue.Close()
+	w.outputQueue.Close()
+}
+
+func (w *CurrencyConverterWorker) handleMessage(msg middleware.Message, ack, nack func()) {
+	moneyLaundering, err := serializer.DeserializeMoneyLaundering(msg)
+	if err != nil {
+		nack()
+		return
+	}
+
+	switch moneyLaundering.GetType() {
+	case protobuf.MessageType_TO_CONVERT_TYPE_FILTERED_PAYMENT:
+		w.handleConvertMessage(moneyLaundering, ack, nack)
+	case protobuf.MessageType_EOF_:
+		w.handleEOFMessage(msg, ack, nack)
+	default:
+		nack()
+	}
+}
+
+func (w *CurrencyConverterWorker) handleConvertMessage(moneyLaundering *protobuf.MoneyLaundry, ack, nack func()) {
+	toConvertMsg, err := serializer.DeserializeTransaction(moneyLaundering.GetPayload(), &protobuf.ToConvertTypeFilteredPayment{})
+	if err != nil {
+		nack()
+		return
+	}
+
+	currency := toConvertMsg.GetPaymentCurrency()
+	amount, err := strconv.ParseFloat(toConvertMsg.GetAmountPaid(), 64)
+	if err != nil {
+		nack()
+		return
+	}
+
+	convertedAmount, err := w.converter.ConvertToUSD(currency, amount)
+	if err == ErrorCurrencyNotFound {
+		// SI no se encuentra la moneda, por el momento se filtra
+		ack()
+		return
+	}
+
+	if err := w.sendConvertedMessage(convertedAmount); err != nil {
+		nack()
+		return
+	}
+	ack()
+}
+
+func (w *CurrencyConverterWorker) sendConvertedMessage(convertedAmount float64) error {
 	return nil
+}
+
+func (w *CurrencyConverterWorker) handleEOFMessage(msg middleware.Message, ack, nack func()) {
+	slog.Info("EOF message received")
+	if err := w.outputQueue.Send(msg); err != nil {
+		nack()
+		return
+	}
+	ack()
 }
