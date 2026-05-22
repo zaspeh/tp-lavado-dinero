5723d5eedc7694060be52e2c6e70551e3ee1baa7
ArielFol <folgueiraariel@gmail.com>
2026-05-22 01:53:28 -0300

    feat: agrega constructor de GroupByDestinationWorker

--- internal/workers/groupers/origindestination/group_by_destination_worker.go
+++ internal/workers/groupers/origindestination/group_by_destination_worker.go
@@ -19,3 +19,29 @@ type GroupByDestinationWorkerConfig struct {
 	OutputQueueName   string
 	MaxBatchWeight    int
 }
+
+func NewGroupByDestinationWorker(config GroupByDestinationWorkerConfig) (*GroupByDestinationWorker, error) {
+	connSettings := middleware.ConnSettings{
+		Hostname: config.MomHost,
+		Port:     config.MomPort,
+	}
+
+	inputExchangeKeys := []string{config.InputExchangeName + "." + config.ID}
+	inputExchange, err := middleware.CreateExchangeMiddleware(config.InputExchangeName, inputExchangeKeys, connSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	outputQueue, err := middleware.CreateQueueMiddleware(config.OutputQueueName, connSettings)
+	if err != nil {
+		inputExchange.Close()
+		return nil, err
+	}
+
+	return &GroupByDestinationWorker{
+		inputExchange:     inputExchange,
+		outputQueue:       outputQueue,
+		destinationsStore: newAccountStore(),
+		maxBatchWeight:    config.MaxBatchWeight,
+	}, nil
+}
