65cf39a2301c7727e8872bea9fe04f9216dff467
ArielFol <folgueiraariel@gmail.com>
2026-05-24 02:17:00 -0300

    feat: agrego constructor de ScatterGatherJoinWorker

--- internal/workers/joiners/scattergatherjoin/scattergatherjoin.go
+++ internal/workers/joiners/scattergatherjoin/scattergatherjoin.go
@@ -20,3 +20,38 @@ type ScatterGatherJoinConfig struct {
 	MomPort        int
 	MaxBatchWeight int
 }
+
+func NewScatterGatherJoinWorker(config ScatterGatherJoinConfig) (*ScatterGatherJoinWorker, error) {
+	connSettings := middleware.ConnSettings{
+		Hostname: config.MomHost,
+		Port:     config.MomPort,
+	}
+
+	inputQueue, err := middleware.CreateQueueMiddleware(
+		config.InputQueueName,
+		connSettings,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resultExchange, err := middleware.CreateExchangeMiddleware(
+		config.ClientExchangeName,
+		[]string{config.ClientExchangeName},
+		connSettings,
+	)
+
+	if err != nil {
+		inputQueue.Close()
+		return nil, err
+	}
+
+	return &ScatterGatherJoinWorker{
+		inputQueue:         inputQueue,
+		resultExchange:     resultExchange,
+		clientExchangeName: config.ClientExchangeName,
+		store:              NewScatterGatherStore(),
+		maxBatchWeight:     config.MaxBatchWeight,
+	}, nil
+}
