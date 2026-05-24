6b8019516f733caf0e69dd3231252fec1c92b2b6
ArielFol <folgueiraariel@gmail.com>
2026-05-24 02:22:29 -0300

    feat: manejo de sigterm/sigint en ScatterGatherJoinWorker

--- internal/workers/joiners/scattergatherjoin/scattergatherjoin.go
+++ internal/workers/joiners/scattergatherjoin/scattergatherjoin.go
@@ -1,6 +1,13 @@
 package scattergatherjoin
 
-import "github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
+)
 
 type ScatterGatherJoinWorker struct {
 	inputQueue         middleware.Middleware
@@ -54,4 +61,39 @@ func NewScatterGatherJoinWorker(config ScatterGatherJoinConfig) (*ScatterGatherJ
 		store:              NewScatterGatherStore(),
 		maxBatchWeight:     config.MaxBatchWeight,
 	}, nil
+}
+
+func (sgj *ScatterGatherJoinWorker) Run() error {
+
+	go sgj.handleSignals()
+
+	sgj.inputQueue.StartConsuming(func(msg middleware.Message, ack, nack func()) {
+		sgj.handleMessage(msg, ack, nack)
+	})
+
+	//TODO: REvisar SI HAY FORMA DE RETORNAR ERRORES
+
+	return nil
+}
+
+func (sgj *ScatterGatherJoinWorker) handleSignals() {
+
+	signals := make(chan os.Signal, 1)
+
+	signal.Notify(
+		signals,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+
+	<-signals
+
+	slog.Info("shutdown signal received")
+
+	sgj.inputQueue.Close()
+	sgj.resultExchange.Close()
+}
+
+func (sgj *ScatterGatherJoinWorker) handleMessage(msg middleware.Message, ack, nack func()) {
+	//TODO
 }
