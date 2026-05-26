74a5970a157c997491647b1ffea297f492acb137
Andres Colina <acolina@fi.uba.ar>
2026-05-25 23:44:11 -0300

    feat: batching y coordinacion en nodo amount q5

--- internal/workers/filters/conversionamountfilter/amountfilter_worker.go
+++ internal/workers/filters/conversionamountfilter/amountfilter_worker.go
@@ -6,15 +6,19 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/batch"
 	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/middleware"
 	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/protobuf"
-	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/serializer"
+	"github.com/zaspeh/tp-lavado-dinero/internal/common/inner/protobuf/protowrappers"
+	c "github.com/zaspeh/tp-lavado-dinero/internal/workers/eofcoordinator"
 )
 
 type AmountFilterWorker struct {
 	inputQueue     middleware.Middleware
 	outputQueue    middleware.Middleware
 	AmountToFilter float64
+	coordinator    *c.EOFCoordinator
+	batchers       map[string]*batch.Batcher[*protobuf.ConvertedAmount, *protobuf.ConvertedAmountBatch]
 }
 
 type AmountFilterConfig struct {
@@ -23,6 +27,9 @@ type AmountFilterConfig struct {
 	MomHost         string
 	MomPort         int
 	AmountToFilter  float64
+	WorkerID        int
+	WorkerCount     int
+	WorkerExchange  string
 }
 
 func NewAmountFilter(config AmountFilterConfig) (*AmountFilterWorker, error) {
@@ -42,15 +49,35 @@ func NewAmountFilter(config AmountFilterConfig) (*AmountFilterWorker, error) {
 		return nil, err
 	}
 
-	return &AmountFilterWorker{
+	worker := &AmountFilterWorker{
 		inputQueue:     inputQueue,
 		outputQueue:    outputQueue,
 		AmountToFilter: config.AmountToFilter,
-	}, nil
+		batchers:       make(map[string]*batch.Batcher[*protobuf.ConvertedAmount, *protobuf.ConvertedAmountBatch]),
+	}
+
+	coordinatorConfig := c.EOFCoordinatorConfig{
+		PeersExchangeName: config.WorkerExchange,
+		ConnSettings:      connSettings,
+		WorkerID:          config.WorkerID,
+		WorkerCount:       config.WorkerCount,
+		FlushHandler:      worker.sendEOFMessage,
+	}
+
+	coordinator, err := c.NewEOFCoordinator(coordinatorConfig)
+	if err != nil {
+		inputQueue.Close()
+		outputQueue.Close()
+		return nil, err
+	}
+
+	worker.coordinator = coordinator
+	return worker, nil
 }
 
 func (af *AmountFilterWorker) Run() error {
 	go af.handleSignals()
+	go af.coordinator.Run()
 	err := af.inputQueue.StartConsuming(func(msg middleware.Message, ack, nack func()) {
 		af.handleMessage(msg, ack, nack)
 
@@ -69,48 +96,136 @@ func (w *AmountFilterWorker) handleSignals() {
 }
 
 func (af *AmountFilterWorker) handleMessage(msg middleware.Message, ack, nack func()) {
-	moneyLaundering, err := serializer.DeserializeMoneyLaundering(msg)
+	moneyLaundering, err := protobuf.DeserializeMoneyLaunderingONTRIAL(msg)
 	if err != nil {
 		nack()
 		return
 	}
 
 	switch moneyLaundering.GetType() {
-	case protobuf.MessageType_CONVERTED_AMOUNT:
-		af.handleConvertedAmountMessage(moneyLaundering, msg, ack, nack)
+	case protobuf.MessageType_CONVERTED_AMOUNT_BATCH:
+		af.handleConvertedAmountBatch(moneyLaundering, ack, nack)
 	case protobuf.MessageType_EOF_:
-		af.handleEOFMessage(msg, ack, nack)
+		af.handleEOFMessage(moneyLaundering, ack, nack)
 	default:
 		nack()
 	}
 }
 
-func (af *AmountFilterWorker) handleEOFMessage(msg middleware.Message, ack, nack func()) {
-	slog.Info("EOF message received")
-	if err := af.outputQueue.Send(msg); err != nil {
+func (af *AmountFilterWorker) handleEOFMessage(moneyLaundering *protobuf.MoneyLaundry, ack, nack func()) {
+	clientID := moneyLaundering.GetClientID()
+	if batcher := af.batchers[clientID]; batcher != nil {
+		if err := batcher.Flush(); err != nil {
+			nack()
+			return
+		}
+	}
+
+	eofMessage := moneyLaundering.GetEofMessage()
+	if err := af.coordinator.HandleLocalEOF(clientID, eofMessage.GetTotalTransactions()); err != nil {
 		nack()
 		return
 	}
 	ack()
 }
 
-func (af *AmountFilterWorker) handleConvertedAmountMessage(moneyLaundering *protobuf.MoneyLaundry, msg middleware.Message, ack, nack func()) {
-	convertedAmountMsg, err := serializer.DeserializeTransaction(moneyLaundering.GetPayload(), &protobuf.ConvertedAmount{})
-	if err != nil {
-		nack()
-		return
+func (af *AmountFilterWorker) handleConvertedAmountBatch(moneyLaundering *protobuf.MoneyLaundry, ack, nack func()) {
+	clientID := moneyLaundering.GetClientID()
+	batcher := af.getBatcher(clientID)
+
+	convertedBatch := moneyLaundering.GetConvertedAmountBatch()
+	for _, convertedAmountMsg := range convertedBatch.GetItems() {
+		amount := convertedAmountMsg.GetAmount()
+		if amount >= af.AmountToFilter {
+			if err := af.coordinator.RecordProcessed(clientID); err != nil {
+				nack()
+				return
+			}
+			continue
+		}
+
+		if err := batcher.Add(convertedAmountMsg); err != nil {
+			nack()
+			return
+		}
+		if err := af.coordinator.RecordSurvivor(clientID); err != nil {
+			nack()
+			return
+		}
+		if err := af.coordinator.RecordProcessed(clientID); err != nil {
+			nack()
+			return
+		}
 	}
 
-	amount := convertedAmountMsg.GetAmount()
-	if amount >= af.AmountToFilter {
-		ack()
-		return
-	}
-
-	if err := af.outputQueue.Send(msg); err != nil {
+	if err := batcher.Flush(); err != nil {
 		nack()
 		return
 	}
 
 	ack()
+}
+
+func (af *AmountFilterWorker) getBatcher(clientID string) *batch.Batcher[*protobuf.ConvertedAmount, *protobuf.ConvertedAmountBatch] {
+	if batcher, ok := af.batchers[clientID]; ok {
+		return batcher
+	}
+
+	convertedBatch := batch.New(
+		0,
+		protowrappers.ProtoSizer[*protobuf.ConvertedAmount](),
+		protowrappers.WrapConvertedAmounts,
+	)
+
+	onFlush := func(batch *protobuf.ConvertedAmountBatch) error {
+		return af.sendConvertedBatch(clientID, batch)
+	}
+
+	batcher := batch.NewBatcher(convertedBatch, onFlush)
+	af.batchers[clientID] = batcher
+	return batcher
+}
+
+func (af *AmountFilterWorker) sendConvertedBatch(clientID string, batch *protobuf.ConvertedAmountBatch) error {
+	if len(batch.GetItems()) == 0 {
+		return nil
+	}
+
+	innerMessage := &protobuf.MoneyLaundry_ConvertedAmountBatch{
+		ConvertedAmountBatch: batch,
+	}
+	serializedMsg, err := protobuf.SerializeProtoMessageONTRIAL(
+		clientID,
+		protobuf.MessageType_CONVERTED_AMOUNT_BATCH,
+		innerMessage,
+	)
+	if err != nil {
+		return err
+	}
+
+	return af.outputQueue.Send(serializedMsg)
+}
+
+func (af *AmountFilterWorker) sendEOFMessage(clientID string, newEOFCount uint64) error {
+	if !af.coordinator.IsLeader() {
+		return nil
+	}
+
+	slog.Info("Sending EOF message", "clientID", clientID, "newEOFCount", newEOFCount)
+	eofMessage := &protobuf.MoneyLaundry_EofMessage{
+		EofMessage: &protobuf.EOF{
+			TotalTransactions: newEOFCount,
+		},
+	}
+
+	serializedEOFMessage, err := protobuf.SerializeProtoMessageONTRIAL(
+		clientID,
+		protobuf.MessageType_EOF_,
+		eofMessage,
+	)
+	if err != nil {
+		return err
+	}
+
+	return af.outputQueue.Send(serializedEOFMessage)
 }
