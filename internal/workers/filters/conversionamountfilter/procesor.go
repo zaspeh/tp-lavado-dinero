a0dfd621088274c8fd4da2fc4581a912bd8ad68c
Andres Colina <acolina@fi.uba.ar>
2026-06-02 07:12:52 -0300

    feat: procesador de filtro de conversion

---
+++ internal/workers/filters/conversionamountfilter/procesor.go
@@ -0,0 +1,20 @@
+package conversionamountfilter
+
+import "github.com/zaspeh/tp-lavado-dinero/internal/common/inner/protobuf"
+
+type Processor struct {
+	AmountToFilter float64
+}
+
+func New(amountToFilter float64) *Processor {
+	return &Processor{
+		AmountToFilter: amountToFilter,
+	}
+}
+
+func (p *Processor) Process(clientID string, item *protobuf.ConvertedAmount) ([]*protobuf.ConvertedAmount, error) {
+	if item.Amount >= p.AmountToFilter {
+		return []*protobuf.ConvertedAmount{item}, nil
+	}
+	return nil, nil
+}
