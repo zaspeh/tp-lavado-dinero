4fc20212d2c89a32709322400d94a6459a78ad1a
Andres Colina <acolina@fi.uba.ar>
2026-06-06 15:06:47 -0300

    feat: procesador de filtro de formatos de pago

---
+++ internal/workers/processor/filters/formatfilter.go
@@ -0,0 +1,35 @@
+package filterprocessor
+
+import (
+	"slices"
+
+	protobuf "github.com/zaspeh/tp-lavado-dinero/internal/common/inner/protobuf/protomessages"
+)
+
+type FormatFilterProcessor struct {
+	allowedFormats []string
+}
+
+func NewFormatFilterProcessor(allowedFormats []string) *FormatFilterProcessor {
+	return &FormatFilterProcessor{
+		allowedFormats: allowedFormats,
+	}
+}
+
+func (f *FormatFilterProcessor) Process(msg *protobuf.ToConvertPeriodFiltered) ([]*protobuf.ToConvertTypeFilteredPayment, error) {
+	paymentFormat := msg.GetPaymentFormat()
+
+	if !f.isAllowedFormat(paymentFormat) {
+		return nil, nil
+	}
+	filteredMsg := &protobuf.ToConvertTypeFilteredPayment{
+		AmountPaid:      msg.GetAmountPaid(),
+		PaymentCurrency: msg.GetPaymentCurrency(),
+		Timestamp:       msg.GetTimestamp(),
+	}
+	return []*protobuf.ToConvertTypeFilteredPayment{filteredMsg}, nil
+}
+
+func (f *FormatFilterProcessor) isAllowedFormat(format string) bool {
+	return slices.Contains(f.allowedFormats, format)
+}
