204bf6ec6d27136e32aef1d70a318797cfcb4de7
Andres Colina <acolina@fi.uba.ar>
2026-06-12 14:44:36 -0300

    feat: batch id se envia al hacer onflush en el batcher

--- internal/common/batch/Batcher.go
+++ internal/common/batch/Batcher.go
@@ -8,11 +8,11 @@ import (
 
 type Batcher[T any, V any] struct {
 	batch        *Batch[T, V]
-	onFlush      func(V) error
+	onFlush      func(V, string) error
 	batchFlushed uint
 }
 
-func NewBatcher[T any, V any](b *Batch[T, V], onFlush func(V) error) *Batcher[T, V] {
+func NewBatcher[T any, V any](b *Batch[T, V], onFlush func(V, string) error) *Batcher[T, V] {
 	return &Batcher[T, V]{batch: b, onFlush: onFlush}
 }
 
@@ -32,7 +32,7 @@ func (s *Batcher[T, V]) SetNewBatchId(batchID string) {
 
 func (s *Batcher[T, V]) Add(item T) error {
 	if !s.batch.TryAdd(item) {
-		if err := s.onFlush(s.batch.Flush()); err != nil {
+		if err := s.onFlush(s.batch.Flush(), s.batch.id); err != nil {
 			return err
 		}
 		if !s.batch.TryAdd(item) {
@@ -49,5 +49,5 @@ func (s *Batcher[T, V]) Flush() error {
 	if s.batch.IsEmpty() {
 		return nil
 	}
-	return s.onFlush(s.batch.Flush())
+	return s.onFlush(s.batch.Flush(), s.batch.id)
 }
