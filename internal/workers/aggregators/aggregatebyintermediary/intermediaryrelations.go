f2984acccdc3366c116c8a0453950c89026127c0
ArielFol <folgueiraariel@gmail.com>
2026-06-22 19:43:44 -0300

    chore: vuelo archivos viejos

--- internal/workers/aggregators/aggregatebyintermediary/intermediaryrelations.go
+++
@@ -1,8 +0,0 @@
-package aggregatebyintermediary
-
-import "github.com/zaspeh/tp-lavado-dinero/internal/common/inner/model"
-
-type IntermediaryRelations struct {
-	Origins      map[model.Account]struct{}
-	Destinations map[model.Account]struct{}
-}
